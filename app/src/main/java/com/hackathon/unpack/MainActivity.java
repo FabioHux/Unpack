bba6b1591218a6b38f046f173b01091627df1be3
FabioHux <huxfabio@gmail.com>
2021-04-17 19:14:59 -0400

    Baseline for Vincent

--- app/src/main/java/com/hackathon/unpack/MainActivity.java
+++ app/src/main/java/com/hackathon/unpack/MainActivity.java
@@ -2,7 +2,9 @@ package com.hackathon.unpack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openQuestionnaire(View view){
+        startActivity(new Intent(this, Questionnaire.class));
+    }
+
+    public void openResources(View view){
+        startActivity(new Intent(this, ResourceList.class));
     }
 }
---
+++ app/src/main/java/com/hackathon/unpack/ResourceList.java
@@ -0,0 +1,14 @@
+package com.hackathon.unpack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResourceList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resource_list);
+    }
+}
